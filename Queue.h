52d062702e32abbd4c492b89e46c658fea1b336b
Hongsheng Fu <opiiopo@outlook.com>
2020-07-27 08:54:13 +0800

    Queue ADT

---
+++ Queue.h
@@ -0,0 +1,22 @@
+// Queue.h ... interface to Queue ADT
+// Written by John Shepherd, March 2013
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#define ItemCopy(i) (i)
+#define ItemEQ(i1, i2) ((i1) == (i2))
+#define ItemShow(i) printf ("%d", (i))
+
+typedef int Vertex;
+typedef Vertex Item;
+typedef struct QueueRep *Queue;
+
+Queue newQueue (void);			// create new empty queue
+void dropQueue (Queue);			// free memory used by queue
+void showQueue (Queue);			// display as 3 > 5 > 4 > ...
+void QueueJoin (Queue, Item);	// add item on queue
+Item QueueLeave (Queue);		// remove item from queue
+int QueueIsEmpty (Queue);		// check for no items
+
+#endif
